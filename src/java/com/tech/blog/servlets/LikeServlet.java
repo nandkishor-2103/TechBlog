8b4e237a1472ac1a0e3f778ed4dfdd6b7353419b
Tabish Firoz <mdtabishfiroz04@gmail.com>
2025-10-26 15:42:46 +0530

    Like, Login, Logout Servlet Added Successfully

--- src/java/com/tech/blog/servlets/LikeServlet.java
+++ src/java/com/tech/blog/servlets/LikeServlet.java
@@ -1,15 +1,15 @@
 package com.tech.blog.servlets;
 
-import com.tech.blog.entities.Message;
+import com.tech.blog.dao.LikeDao;
+import com.tech.blog.helper.ConnectionProvider;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-public class LogoutServlet extends HttpServlet {
+public class LikeServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -25,25 +25,19 @@ public class LogoutServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LogoutServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
+            String operation = request.getParameter("operation");
+            int uid = Integer.parseInt(request.getParameter("uid"));
+            int pid = Integer.parseInt(request.getParameter("pid"));
 
-            HttpSession s = request.getSession();
-
-            s.removeAttribute("currentUser");
-
-            Message m = new Message("Logout Successfully", "success", "alert-success");
-
-            s.setAttribute("msg", m);
-
-            response.sendRedirect("login_page.jsp");
-
-            out.println("</body>");
-            out.println("</html>");
+            // out.println("data from server");
+            // out.println(operation);
+            // out.println(uid);
+            // out.println(pid);
+            LikeDao ldao = new LikeDao(ConnectionProvider.getConnection());
+            if (operation.equals("like")) {
+                boolean f = ldao.insertLike(pid, uid);
+                out.println(f);
+            }
         }
     }
 
